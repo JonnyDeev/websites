54802b4ac7607312a09df6c0f957cae6679bd008
JonnyDeev <111886518+JonnyDeev@users.noreply.github.com>
2025-04-30 19:02:38 -0400

    Refactor navigation structure and add responsive styles; implement menu toggle functionality

---
+++ Marketing Agency Website/script.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector("#menu-btn");
+const navLinks = document.querySelector("#nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+function handleMenuBtn() {
+  console.log("test");
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+}
+
+menuBtn.addEventListener("click", handleMenuBtn);
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
