f059d470654a82fcf5753f948737495597e49f79
JonnyDeev <111886518+JonnyDeev@users.noreply.github.com>
2025-05-04 16:29:41 -0400

    Update navigation links and enhance layout with new service and about sections; improve styles for responsiveness and visual appeal

--- Marketing Agency Website/script.js
+++ Marketing Agency Website/script.js
@@ -2,6 +2,12 @@ const menuBtn = document.querySelector("#menu-btn");
 const navLinks = document.querySelector("#nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
+const scrollRevealOptions = {
+  distance: "50px",
+  duration: 1500,
+  origin: "bottom",
+}
+
 function handleMenuBtn() {
   console.log("test");
   navLinks.classList.toggle("open");
@@ -17,3 +23,12 @@ navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+
+ScrollReveal().reveal('.header__content h1', {...scrollRevealOptions})
+ScrollReveal().reveal('.section__description ', {...scrollRevealOptions, delay: 500})
+ScrollReveal().reveal('.header__btns', {...scrollRevealOptions,  delay: 600})
+ScrollReveal().reveal('.service__card', {...scrollRevealOptions,  interval: 700})
+ScrollReveal().reveal('.service__btn', {...scrollRevealOptions,  interval: 800})
+ScrollReveal().reveal('.about__container .section__header', {...scrollRevealOptions})
+ScrollReveal().reveal('.about__list li', {...scrollRevealOptions, delay: 500, interval: 500})
