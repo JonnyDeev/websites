4548b9cfc64e4fc02368a69f72b7b49ceb813153
JonnyDeev <111886518+JonnyDeev@users.noreply.github.com>
2025-05-10 15:26:11 -0400

    Finished HTML Structure

--- JERCA/scripts/script.js
+++ JERCA/scripts/script.js
@@ -13,9 +13,26 @@ function menuBtnHandler() {
 
 menuBtn.addEventListener("click", menuBtnHandler);
 
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
-navLinks.addEventListener('click', () => {
-  
-    navLinks.classList.remove('open')
-    menuBtnIcon.setAttribute('class', 'ri-menu-line')
-})
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
